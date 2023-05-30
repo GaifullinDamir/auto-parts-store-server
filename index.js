d192f0fbcbdc0975bd81e3d87d937aefbad550db
Name <damiros.20002@gmail.com>
2023-05-30 21:35:05 +0300

    Проработана регистрация

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { validationResult } from 'express-validator';
 
 import { registerValidation } from './validation/validation.js';
+
+import UserModel from './models/user.js';
 
 dotenv.config();
 
@@ -25,15 +28,46 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-    }
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
 
-    res.json({
-        success: true,
-    })
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            passwordHash: hash,
+            role: req.body.role,
+        })
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+                _id: user._id,
+            }, 
+            _SECRET_KEY,
+            {
+                expiresIn: '30d',
+            },
+        );
+        
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+    } catch(error) {
+        console.log('Registration failed:', error);
+        res.status(500).json({
+            message: 'Registration failed.',
+        });
+    }
 });
 
 app.listen(_PORT, (error) => {
