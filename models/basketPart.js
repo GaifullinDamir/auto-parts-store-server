3ed4b26f1015419856f8fb803b21cce1759705c5
Name <damiros.20002@gmail.com>
2023-06-01 20:39:57 +0300

    Поменял схему basketPart

--- models/basketPart.js
+++ models/basketPart.js
@@ -1,6 +1,31 @@
 import mongoose, { model } from "mongoose";
 
 const BasketPartSchema = new mongoose.Schema({
+    fullname:{
+        type: String,
+        require: true,
+        default: ''
+    },
+    phoneNumber:{
+        type: String,
+        require: true,
+        default: ''
+    },
+    address:{
+        type: String,
+        require: true,
+        default: ''
+    },
+    phoneNumber:{
+        type: String,
+        require: true,
+        default: ''
+    },
+    orderIsPaid:{
+        type: Boolean,
+        required: true,
+        default: false
+    },
     basket: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Basket',
